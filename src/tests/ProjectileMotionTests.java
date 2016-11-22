b9aff637c29258c7ed6e22d1c2038136ae63df48
Andrew Petersen <andy.p@comcast.net>
2016-11-22 11:54:28 -0700

    Added motion test to ProjectileMotionTests

--- src/tests/ProjectileMotionTests.java
+++ src/tests/ProjectileMotionTests.java
@@ -2,13 +2,66 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import game.Eggnog;
+import game.GameFrame;
 
 public class ProjectileMotionTests {
 
+	private static GameFrame gf;
+
+	@BeforeClass
+	public void setup(){
+		gf = GameFrame.getInstance();
+	}
+
 	@Test
 	public void testPositions() {
-		fail("Not yet implemented");
+		Eggnog nogg = gf.getProjectile();
+
+		//multiple projectile position tests
+
+		nogg.setDeltaT(1.0);
+		nogg.setAngle(45.0);
+		nogg.reset();
+
+		//updates motion every dt (in reality dt small, update called from timer every dt seconds)
+		nogg.updateMotion();
+		nogg.updateMotion();
+		nogg.updateMotion();
+
+		Point pos = new Point();
+		pos.setLocation(3.5,4); //predicted position
+		assertEquals(pos, nogg.getPosition());
+
+		//2nd position test
+		nogg.setAngle(25.0);
+		nogg.reset();
+
+		//updates motion every dt (in reality dt small, update called from timer every dt seconds)
+		nogg.updateMotion();
+		nogg.updateMotion();
+		nogg.updateMotion();
+
+		pos.setLocation(3.5,4); //predicted position
+		assertEquals(pos, nogg.getPosition());
+
+		//3rd position test
+		nogg.setAngle(15.0);
+		nogg.reset();
+
+		//updates motion every dt (in reality dt small, update called from timer every dt seconds)
+		nogg.updateMotion();
+		nogg.updateMotion();
+		nogg.updateMotion();
+
+		pos.setLocation(3.5,4); //predicted position
+		assertEquals(pos, nogg.getPosition());
+
 	}
 
 }
