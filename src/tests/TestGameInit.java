402feaba47b539a076e122ee1ad9b35896a6f4b2
Andrew Petersen <andy.p@comcast.net>
2016-11-22 11:24:37 -0700

    Initial failing tests for TestGameInit

--- src/tests/TestGameInit.java
+++ src/tests/TestGameInit.java
@@ -2,13 +2,45 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import game.GameFrame;
 
 public class TestGameInit {
 
+	GameFrame gf;
+	
+	@BeforeClass
+	public void setup(){
+		gf = GameFrame.getInstance();
+	}
+	
 	@Test
-	public void test() {
+	public void drawDisplayTest() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void drawTrajectoryTes() {
+		gf.calculateTrajectory(45.0);
+		ArrayList<Point> pts = gf.getTrajectory();
+	}
+	
+	@Test
+	public void loadImagesTest(){
+		try{
+			gf.loadImages();
+			assert(true);
+		}
+		catch(FileNotFoundException e){
+			assert(false);
+		}
+	}
+	
 
 }
