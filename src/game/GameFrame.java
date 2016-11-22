aa839b3d4c2d432f06049b6ad5ba0ba3bc33fa10
JP Skeath <jskeath@mymail.mines.edu>
2016-11-22 11:29:19 -0700

    Added game sizes to gamFrame class

--- src/game/GameFrame.java
+++ src/game/GameFrame.java
@@ -2,7 +2,10 @@ package game;
 
 public class GameFrame {
 	private static GameFrame gameInstance = new GameFrame();
-
+	
+	//Size of the gameBoard in Pixels
+	public static final int gameXSize = 600;
+	public static final int gameYSize = 600;
 	
 	private GameFrame() {
 		
