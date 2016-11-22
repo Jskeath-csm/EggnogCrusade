41824f86be4ceaa2dafe722c31628172fb7d555c
Andrew Petersen <andy.p@comcast.net>
2016-11-22 12:17:42 -0700

    added more motion tests

--- src/tests/ProjectileMotionTests.java
+++ src/tests/ProjectileMotionTests.java
@@ -7,6 +7,7 @@ import java.awt.Point;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import game.Bucket;
 import game.Eggnog;
 import game.GameFrame;
 
@@ -21,47 +22,84 @@ public class ProjectileMotionTests {
 
 	@Test
 	public void testPositions() {
-		Eggnog nogg = gf.getProjectile();
+		Eggnog nog = gf.getProjectile();
 
 		//multiple projectile position tests
 
-		nogg.setDeltaT(1.0);
-		nogg.setAngle(45.0);
-		nogg.reset();
+		nog.setDeltaT(1.0);
+		nog.setAngle(45.0);
+		nog.reset();
 
 		//updates motion every dt (in reality dt small, update called from timer every dt seconds)
-		nogg.updateMotion();
-		nogg.updateMotion();
-		nogg.updateMotion();
+		nog.updateMotion();
+		nog.updateMotion();
+		nog.updateMotion();
 
 		Point pos = new Point();
 		pos.setLocation(3.5,4); //predicted position
-		assertEquals(pos, nogg.getPosition());
+		assertEquals(pos, nog.getPosition());
 
 		//2nd position test
-		nogg.setAngle(25.0);
-		nogg.reset();
+		nog.setAngle(25.0);
+		nog.reset();
 
 		//updates motion every dt (in reality dt small, update called from timer every dt seconds)
-		nogg.updateMotion();
-		nogg.updateMotion();
-		nogg.updateMotion();
+		nog.updateMotion();
+		nog.updateMotion();
+		nog.updateMotion();
 
 		pos.setLocation(3.5,4); //predicted position
-		assertEquals(pos, nogg.getPosition());
+		assertEquals(pos, nog.getPosition());
 
 		//3rd position test
-		nogg.setAngle(15.0);
-		nogg.reset();
+		nog.setAngle(15.0);
+		nog.reset();
 
 		//updates motion every dt (in reality dt small, update called from timer every dt seconds)
-		nogg.updateMotion();
-		nogg.updateMotion();
-		nogg.updateMotion();
+		nog.updateMotion();
+		nog.updateMotion();
+		nog.updateMotion();
 
 		pos.setLocation(3.5,4); //predicted position
-		assertEquals(pos, nogg.getPosition());
+		assertEquals(pos, nog.getPosition());
+	}
+	
+	@Test
+	public void testOutOfBounds(){
+		Eggnog nog = gf.getProjectile();
 
+		//multiple projectile position tests
+
+		nog.setDeltaT(100.0); //large deltaT, definitely will go off screen after 1 update
+		nog.setAngle(45.0);
+		nog.reset();
+		
+		nog.updateMotion();
+		
+		assertFalse(nog.getVisible());
+	}
+	
+	@Test
+	public void testCollision(){
+		Eggnog nog = gf.getProjectile();
+		Bucket b = new Bucket();
+		b.setPosition(new Point(3,0));
+		
+		//multiple projectile position tests
+
+		nog.setDeltaT(0.030); //roughly 33FPS
+		nog.setAngle(15.0); //low enough to hit wall
+		nog.reset(); //resets position
+
+		assertEquals(gf.getScore(),0);
+		
+		//3 seconds of travel time, had to hit wall
+		for(int i=0;i<100;i++){
+			nog.update();
+		}
+		
+		assertFalse(nog.getVisible());
+		assertTrue(gf.getScore() > 0); //need to change to actual scor value once decided
 	}
 
 }
